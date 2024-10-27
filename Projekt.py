1980582e47fd07ac1872578dd7565a3ae77a10ae
Igorsta <igr.staszkiewicz@gmail.com>
2024-10-27 14:00:52 +0100

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def csv_operacje(sciezki, czy_odczyt):
     #kod dla plików w formacie csv
     pass
@@ -16,19 +17,18 @@ def generuj_strukture_plików(miesiące, dnie, czasy=None):
     return result
 
 def main():
-    all_months = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", 
-                  "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
-    all_days = ["pn", "wt", "sr", "czw", "pt", "sb", "nd"]
-    all_ranges = ["pn", "wt", "sr", "czw", "pt", "sb", "nd",
-                  "pn-wt", "pn-sr", "pn-czw", "pn-pt", "pn-sb", "pn-nd",
-                  "wt-sr", "wt-czw", "wt-pt", "wt-sb", "wt-nd",
-                  "sr-czw", "sr-pt", "sr-sb", "sr-nd",
-                  "czw-pt", "czw-sb", "czw-nd",
-                  "pt-sb", "pt-nd",
-                  "sb-nd"]
+    all_months  =  ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", 
+                    "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
+    all_days    =  ["pn", "wt", "sr", "czw", "pt", "sb", "nd"]
+    all_ranges  =  ["pn", "wt", "sr", "czw", "pt", "sb", "nd",
+                    "pn-wt", "pn-sr", "pn-czw", "pn-pt", "pn-sb", "pn-nd",
+                    "wt-sr", "wt-czw", "wt-pt", "wt-sb", "wt-nd",
+                    "sr-czw", "sr-pt", "sr-sb", "sr-nd",
+                    "czw-pt", "czw-sb", "czw-nd",
+                    "pt-sb", "pt-nd",
+                    "sb-nd"]
     all_times = ["r", "w"]
-    default_time = 'r'
-    
+
     parser = argparse.ArgumentParser(description="Odczyta wartość z plików o zadanej strukturze, lub je stworzy")
     
     parser.add_argument(
@@ -43,7 +43,7 @@ def main():
 
     parser.add_argument(
         '--czasy', nargs='*', type=str, choices=all_times, default=None,
-        help=f"Opcjonalna lista czasów (w formacie {all_times}). Domyślna wartość to {default_time}."
+        help=f"Opcjonalna lista czasów (w formacie {all_times}). Domyślna wartość to 'rano'."
     )
 
     parser.add_argument(
@@ -58,10 +58,12 @@ def main():
     
     args = parser.parse_args()
     
-    struktura= generuj_strukture_plików(args.months, args.days, args.times)
+    struktura= generuj_strukture_plików(args.miesiące, args.dnie, args.czasy)
     
     if args.csv:
         csv_operacje(struktura, args.tworzenie)
     else:
         json_operacje(struktura, args.tworzenie)
     
+if __name__ == '__main__':
+    main()
