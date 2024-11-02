b3085f84be123c932d1e4a46c779cb98848c67d9
Igorsta <igr.staszkiewicz@gmail.com>
2024-11-02 16:21:47 +0100

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -44,7 +44,7 @@ def main():
                     'Kolejność plików taka jak zadana przez użytkownika.\n' +
                     'Nie należy powielać ścieżek dla pliku w tych samych podkatalogach.\n' +
                     'Nie należy odczytywać nieistniejących plików lub tworzyć już istniejących.\n' + 
-                    'Jeśli jeśli nie zostaną spełnione wymogi wejścia, program będzie wstrzymany i nie zrobi zmian w strukturze.',
+                    'Jeśli jeśli nie zostaną spełnione wymogi wejścia, program będzie wstrzymany i nie zrobi zmian w strukturze.\n',
         formatter_class=argparse.RawTextHelpFormatter
     )
     
@@ -53,7 +53,7 @@ def main():
         help=   "Lista miesięcy do obsłużenia.\n"
                 + 'Nie może być pusta. Miesiące należy podawać osobno. Mogą się powtarzać.\n'
                 + f'Dopuszczone wartości dla nazwy miesięca to:\n\t{", ".join(months_option)}\n'
-                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(months_full_name)}',
+                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(months_full_name)}\n',
         metavar = ''
     )
     
@@ -64,7 +64,7 @@ def main():
                 + f'Można podać pojedynczy dzień, gdzie dopuszczone wartości dla nazwy dnia to:\n\t{", ".join(days_option)}\n'
                 + 'Można też podać zakres dni (w postaci {od którego dnia}-{do którego dnia} włącznie) dla których mają być obsłużone pliki np. pn-czw, sr-nd.\n'
                 + 'Kolejność dni jak powyżej.\n'
-                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(days_full_name)}',
+                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(days_full_name)}\n',
         metavar=''    
     )
 
@@ -73,7 +73,7 @@ def main():
         help=   "Opcjonalna lista pór dnia.\n" 
                 + "Nie może być pusta. Jej długość nie powinna przekraczać ilości plików do obsłużenia.\n"
                 + f"Dopuszczone wartości dla nazwy pory dnia to:\n\t{', '.join(times_option)}\n"
-                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(times_full_name)}'
+                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(times_full_name)}\n'
                 + "Można podać wartości tylko dla początkowych plików. Domyślna wartość to 'r'.\n",
         metavar=''
     )
@@ -82,14 +82,14 @@ def main():
         '-t', '--tworzenie', action='store_true', 
         help=   "Opcjonalna flaga.\n"
                 + "Jeśli dodana do polecenia, pliki będą tworzone.\n"
-                + "Domyślnie pliki są odczytywane."
+                + "Domyślnie pliki są odczytywane.\n"
     )
 
     parser.add_argument(
         '-c', '--csv', action='store_true', 
         help=   "Opcjonalna flaga.\n"
                 + "Jeśli dodana do polecenia, format plików będzie csv.\n"
-                + "Domyślny format plików to json."
+                + "Domyślny format plików to json.\n"
     )
 
     args = parser.parse_args()
@@ -107,6 +107,5 @@ def main():
     if not correct_exec:
         raise argparse.ArgumentError(None, "Brak podanego pliku (dla odczytu) lub Podany plik już istnieje (dla tworzenia)")
         
-    
 if __name__ == '__main__':
     main()
