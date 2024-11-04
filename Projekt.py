fb96fdd50c36998997232defeddb90f6b5f95914
Igorsta <igr.staszkiewicz@gmail.com>
2024-11-04 19:35:20 +0100

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -7,16 +7,16 @@ import os
 import random
 import csv
 import json
+import re
 
-times_full_name  = ["rano", "wieczorem"]
-days_full_name   = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"]
-months_full_name = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", 
-                    "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
+TIMES = dict({'r':'rano', 'w': 'wieczorem'})
 
-times_option   =  ["r", "w"]
-days_option    =  ["pn", "wt", "sr", "czw", "pt", "sb", "nd"]
-months_option  =  ["sty", "lut", "mar", "kwi", "maj", "czer", 
-                   "lip", "sie", "wrz", "paz", "lis", "gru"]
+DAYS  = dict({"pn": "poniedziałek", "wt": "wtorek", "sr": "środa", 
+                "czw": "czwartek", "pt": "piątek", "sb":"sobota", "nd":"niedziela"})
+
+MONTHS= dict({"sty":"styczeń", "lut":"luty", "mar":"marzec", "kwi":"kwiecień", 
+                "maj":"maj", "czer":"czerwiec", "lip":"lipiec", "sie":"sierpień", 
+                "wrz":"wrzesień", "paz":"październik", "lis":"listopad", "gru":"grudzień"})
 
 def csv_operacje(sciezki, czy_odczyt) -> bool:
     #kod dla plików w formacie csv
@@ -59,7 +59,7 @@ def csv_operacje(sciezki, czy_odczyt) -> bool:
     return True
 
 def json_operacje(sciezki, czy_odczyt) -> bool:
-        if czy_odczyt:
+    if czy_odczyt:
         suma_sekund = 0
         for sciezka in sciezki:
             if not os.path.exists(sciezka):
@@ -100,9 +100,9 @@ def generuj_strukture_plików(miesiące, dnie, pory=None):
         dni_range = parse_dni_range(dnie[idx])
         for dzień in dni_range:
             pora_dnia = pory[idx] if idx < len(pory) else 'r'
-            folder_path = os.path.join(months_full_name[months_option.index(miesiąc)],
-                                       days_full_name[days_option.index(dzień)],
-                                       times_full_name[times_option.index(pora_dnia)])
+            folder_path = os.path.join(MONTHS[miesiąc],
+                                       DAYS[dzień],
+                                       TIMES[pora_dnia])
             struktura.append(folder_path)
 
     # Sprawdzenie unikalności ścieżek
@@ -115,6 +115,7 @@ def parse_dni_range(dni):
     """
     Parsuje zakres dni do listy dni.
     """
+    days_option = DAYS.keys()
     if '-' in dni:
         start, end = dni.split('-')
         start_index = days_option.index(start)
@@ -199,23 +200,22 @@ def odczyt_plik_json(sciezka):
     return suma_czasu
 
 def main():
-    zipl        = lambda x, y: list(zip(x, y))
-    mapl        = lambda f, l: list(map(f, l))
-    ranging     = lambda options: options + [x + '-' + y for x, y in  combinations(options, 2)]
-    aliasing    = lambda full, alias: mapl(lambda t: "(" + t[0] + " AS '" + t[1] + "')", zipl(full, alias))
+    months_option= list(MONTHS.keys())
+    times_option = list(TIMES.keys())
+    days_option  = list(DAYS.keys())
 
-    day_ranges          = ranging(days_option)
+    months_full_name= list(MONTHS.values())
+    times_full_name = list(TIMES.values())
+    days_full_name  = list(DAYS.values())
 
-    list_od_month_alias = aliasing(months_full_name, months_option)
-    list_of_time_alias  = aliasing(times_full_name, times_option)
-    list_of_day_alias   = aliasing(days_full_name, days_option)
+    day_ranges   = days_option + [x + '-' + y for x, y in  combinations(days_option, 2)]
 
     parser = argparse.ArgumentParser(
         description="Odczyta wartość z plików o zadanej strukturze lub je stworzy.\n" + 
                     'Kolejność plików taka jak zadana przez użytkownika.\n' +
                     'Nie należy powielać ścieżek dla pliku w tych samych podkatalogach.\n' +
                     'Nie należy odczytywać nieistniejących plików lub tworzyć już istniejących.\n' + 
-                    'Jeśli jeśli nie zostaną spełnione wymogi wejścia, program będzie wstrzymany i nie zrobi zmian w strukturze.\n',
+                    'Jeśli jeśli nie zostaną spełnione wymogi, program przerwie wykonywanie i nie dokończy odczytywania/tworzenia plików.\n',
         formatter_class=argparse.RawTextHelpFormatter
     )
     
@@ -224,7 +224,7 @@ def main():
         help=   "Lista miesięcy do obsłużenia.\n"
                 + 'Nie może być pusta. Miesiące należy podawać osobno. Mogą się powtarzać.\n'
                 + f'Dopuszczone wartości dla nazwy miesięca to:\n\t{", ".join(months_option)}\n'
-                + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(months_full_name)}\n',
+                + f'Program będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(months_full_name)}\n',
         metavar = ''
     )
     
@@ -233,7 +233,7 @@ def main():
         help=  'Zakresy dni do obsłużenia dla każdego miesiąca.\n'
                 + "Ma być tyle samo zakresów co miesięcy.\n"
                 + f'Można podać pojedynczy dzień, gdzie dopuszczone wartości dla nazwy dnia to:\n\t{", ".join(days_option)}\n'
-                + 'Można też podać zakres dni (w postaci {od którego dnia}-{do którego dnia} włącznie) dla których mają być obsłużone pliki np. pn-czw, sr-nd.\n'
+                + 'Można też podać zakres dni (w postaci {od którego dnia}-{do którego dnia} włącznie)\ndla których mają być obsłużone pliki np. pn-czw, sr-nd.\n'
                 + 'Kolejność dni jak powyżej.\n'
                 + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(days_full_name)}\n',
         metavar=''    
@@ -242,7 +242,7 @@ def main():
     parser.add_argument(
         '-p', '--pora', nargs='+', type=str, choices=times_option, default=None,
         help=   "Opcjonalna lista pór dnia.\n" 
-                + "Nie może być pusta. Jej długość nie powinna przekraczać ilości plików do obsłużenia.\n"
+                + "Nie może być pusta. Jej długość nie może przekraczać liczby miesięcy\n"
                 + f"Dopuszczone wartości dla nazwy pory dnia to:\n\t{', '.join(times_option)}\n"
                 + f'Struktura będzie obsługiwać odpowiednio podkatalogi o nazwach:\n\t{", ".join(times_full_name)}\n'
                 + "Można podać wartości tylko dla początkowych plików. Domyślna wartość to 'r'.\n",
